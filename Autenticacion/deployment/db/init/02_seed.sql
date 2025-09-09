dfc64a7880efa67a652aec8f92d8258521223b8a
Miguel Andres Mosquera <mandresmosquera@gmail.com>
2025-09-09 17:16:03 -0600

    Correcion de dockerCompose para levantar el proyecto desde el root de todos los proyectos

--- Autenticacion/deployment/db/init/02_seed.sql
+++ Autenticacion/deployment/db/init/02_seed.sql
@@ -10,4 +10,4 @@ VALUES(3, 'cliente', 'Este es un cliente') ON CONFLICT DO NOTHING;
 
 INSERT INTO public.usuario
 (usuario_id, nombres, apellidos, fecha_nacimiento, direccion, telefono, correo_electronico, salario_base, rol_id, contrasenia)
-VALUES(1, 'Miguel Asesor', 'Mosquera', '1990-05-12', 'Cra 10 # 5-21, Popayán, Cauca', '+57 3001234567', 'asesor@gmail.com', 2500000.00, 2, '$2a$10$P8TscSTcE3.3p2gt2R9P1OJTImGAlNrgeyigYLKE5cf05AyT/Hphm');
+VALUES(1, 'Miguel Asesor', 'Mosquera', '1990-05-12', 'Cra 10 # 5-21, Popayán, Cauca', '+57 3001234567', 'asesor@gmail.com', 2500000.00, 1, '$2a$10$P8TscSTcE3.3p2gt2R9P1OJTImGAlNrgeyigYLKE5cf05AyT/Hphm');
