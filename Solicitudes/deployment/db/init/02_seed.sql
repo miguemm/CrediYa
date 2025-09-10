fcf555036f5fe182c6d90ed873497057941bc5f4
Miguel Andres Mosquera <mandresmosquera@gmail.com>
2025-09-09 21:32:40 -0600

    Estado de solicitud adicionales

--- Solicitudes/deployment/db/init/02_seed.sql
+++ Solicitudes/deployment/db/init/02_seed.sql
@@ -1,3 +1,5 @@
 INSERT INTO public.estado (nombre, descripcion) VALUES('Pendiente de revisión', 'Su solicitud esta pendiente de ser revisada por el sistema.') ON CONFLICT DO NOTHING;
+INSERT INTO public.estado (nombre, descripcion) VALUES('Aprobada', 'Su solicitud fue aprobada.') ON CONFLICT DO NOTHING;
+INSERT INTO public.estado (nombre, descripcion) VALUES('Rechazada', 'Su Solicitud fue rechazada.') ON CONFLICT DO NOTHING;
 
 INSERT INTO public.tipo_prestamo (nombre, monto_minimo, monto_maximo, tasa_interes, validacion_automatica) VALUES('Crédito libre inversión', 5000000.00, 20000000.00, 0.5000, false) ON CONFLICT DO NOTHING;
