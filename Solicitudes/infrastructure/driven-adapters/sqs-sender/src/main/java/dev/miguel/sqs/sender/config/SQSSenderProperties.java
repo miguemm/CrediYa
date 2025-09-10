d7dc53ac3d9f85307d529331cbc495a14a5376c9
Miguel Andres Mosquera <mandresmosquera@gmail.com>
2025-09-10 16:17:41 -0600

    Actualizacion de variables de entorno para SQS

--- Solicitudes/infrastructure/driven-adapters/sqs-sender/src/main/java/dev/miguel/sqs/sender/config/SQSSenderProperties.java
+++ Solicitudes/infrastructure/driven-adapters/sqs-sender/src/main/java/dev/miguel/sqs/sender/config/SQSSenderProperties.java
@@ -2,7 +2,7 @@ package dev.miguel.sqs.sender.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties(prefix = "adapter.sqs")
+@ConfigurationProperties(prefix = "sqs")
 public record SQSSenderProperties(
      String region,
      String queueUrl,
